68af544bdc2fe97dd998fb70a0aa632fb668dea8
Тёхта Назарий Александрович <Tekhta.N.A@sberbank.ru>
2021-10-21 21:15:21 +0300

    lightning

--- architecture/head/TableAttention.py
+++ architecture/head/TableAttention.py
@@ -1,13 +1,10 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
 import numpy as np
-from data.pubtabnet import PubTabNet
 
 
-class TableAttention(pl.LightningModule):
+class TableAttention(nn.Module):
     def __init__(
             self,
             input_channels: int,
@@ -70,28 +67,6 @@ class TableAttention(pl.LightningModule):
         structure_prob = self.generate_structure(rnn_outputs)
         loc_pred = self.generate_loc(rnn_outputs)
         return structure_prob, loc_pred
-
-    def training_step(self, batch, batch_idx):
-        image, (gt_struct, gt_bbox) = batch
-        pred_struct, pred_bbox = self.forward(image)
-        loss_bbox = F.mse_loss(pred_bbox, gt_bbox)
-        loss_struct = F.binary_cross_entropy(pred_struct, gt_struct)
-        self.log('train bbox loss', loss_bbox)
-        self.log('train struct loss', loss_struct)
-        loss = loss_bbox + loss_struct
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
-
-    def train_dataloader(self):
-        ptn_dataset = PubTabNet(
-            '/home/Tekhta/PaddleOCR/data/pubtabnet/PubTabNet_val_span.jsonl',
-            '/home/Tekhta/PaddleOCR/data/pubtabnet/val/',
-            elem_dict_path='/home/Tekhta/TableEDD/utils/dict/table_elements.txt'
-        )
-        return DataLoader(ptn_dataset, batch_size=8, shuffle=True)
 
 
 class AttentionGRU(nn.Module):
--- table_parser.py
+++ table_parser.py
@@ -1,13 +1,18 @@
+import torch
 from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
 from architecture.head.TableAttention import TableAttention
 from torchvision.models import mobilenet_v3_small
+import pytorch_lightning as pl
+from data.pubtabnet import PubTabNet
 
 
 def remove_layers(model, i):
     return nn.Sequential(*list(model.children())[i])
 
 
-class TableEDD(nn.Module):
+class TableEDD(pl.LightningModule):
     def __init__(self, hidden_size, elem_num, max_elem_length, pretrained=False):
         super().__init__()
         self.backbone = remove_layers(mobilenet_v3_small(pretrained=pretrained), 0)
@@ -20,8 +25,29 @@ class TableEDD(nn.Module):
             max_elem_length,
         )
 
-    def forward(self, tensor, target=None):
+    def forward(self, tensor):
         emb = self.backbone(tensor)
         result = self.head(emb)
         return result
 
+    def training_step(self, batch, batch_idx):
+        image, (gt_struct, gt_bbox) = batch
+        pred_struct, pred_bbox = self.forward(image)
+        loss_bbox = F.mse_loss(pred_bbox, gt_bbox)
+        loss_struct = F.binary_cross_entropy(pred_struct, gt_struct)
+        self.log('train bbox loss', loss_bbox)
+        self.log('train struct loss', loss_struct)
+        loss = loss_bbox + loss_struct
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def train_dataloader(self):
+        ptn_dataset = PubTabNet(
+            '/home/Tekhta/PaddleOCR/data/pubtabnet/PubTabNet_val_span.jsonl',
+            '/home/Tekhta/PaddleOCR/data/pubtabnet/val/',
+            elem_dict_path='/home/Tekhta/TableEDD/utils/dict/table_elements.txt'
+        )
+        return DataLoader(ptn_dataset, batch_size=8, shuffle=True)
