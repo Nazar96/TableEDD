d1c9d445545b90555c5bdca1d88b986851c42827
Тёхта Назарий Александрович <Tekhta.N.A@sberbank.ru>
2021-10-24 22:04:19 +0300

    forward fix

--- architecture/head/TableAttention.py
+++ architecture/head/TableAttention.py
@@ -31,11 +31,8 @@ class TableAttention(nn.Module):
             inputs = torch.transpose(inputs, dim0=1, dim1=2)
         return inputs
 
-    def __one_hot(self, inputs):
-        return F.one_hot(inputs.type(torch.int64), self.elem_num)
-
-    def get_elements(self, inputs):
-        elements_prob = self.structure_generator(inputs)
+    def get_elements(self, input):
+        elements_prob = self.structure_generator(input)
         elements = elements_prob.argmax(dim=1)
         return elements
 
@@ -49,19 +46,32 @@ class TableAttention(nn.Module):
         loc_preds = torch.sigmoid(loc_preds)
         return loc_preds
 
-    def forward(self, tensor):
-        tensor = self.__flatten(tensor)
-        batch_size = tensor.shape[0]
-        hidden = torch.zeros((1, batch_size, self.hidden_size))
-        element = torch.zeros(batch_size)
+    def forward(self, input, target=None):
+        device = input.device
+
+        input = self.__flatten(input)
+        batch_size = input.shape[0]
+
+        hidden = torch.zeros((1, batch_size, self.hidden_size)).to(device)
+        element = torch.zeros(batch_size).to(device)
+
         rnn_outputs = []
-        for _ in range(self.max_elem_length):
-            element_ohe = self.__one_hot(element)
-            output, hidden, alpha = self.structure_attention(
-                tensor, hidden, element_ohe
-            )
-            element = self.get_elements(torch.squeeze(output, dim=1))
-            rnn_outputs.append(output)
+        if target is not None:
+            # use_teacher_forcing
+            for i in range(len(target)):
+                output, hidden, alpha = self.structure_attention(
+                    element, hidden, input
+                )
+                element = target[i]
+                rnn_outputs.append(output)
+
+        else:
+            for _ in range(self.max_elem_length):
+                output, hidden, alpha = self.structure_attention(
+                    element, hidden, input
+                )
+                element = self.get_elements(torch.squeeze(output, dim=1)).detach()
+                rnn_outputs.append(output)
 
         rnn_outputs = torch.cat(rnn_outputs, dim=1)
         structure_prob = self.generate_structure(rnn_outputs)
@@ -81,11 +91,13 @@ class AttentionGRU(nn.Module):
         self.h2h = nn.Linear(hidden_size, hidden_size)
         self.score = nn.Linear(hidden_size, 1, bias=False)
         self.rnn = nn.GRU(input_size+num_embeddings, hidden_size, batch_first=True)
+        self.embedding = nn.Embedding(num_embeddings)
 
-    def forward(self, tensor, hidden_0, char_ohe):
+    def forward(self, input, hidden, encoder_output):
+        input = self.embedding(input)
 
-        input_proj = self.i2h(tensor)
-        h0_proj = self.h2h(hidden_0)
+        input_proj = self.i2h(encoder_output)
+        h0_proj = self.h2h(hidden)
         h0_proj = torch.transpose(h0_proj, dim0=1, dim1=0)
 
         res = torch.tanh(input_proj+h0_proj)
@@ -93,9 +105,9 @@ class AttentionGRU(nn.Module):
         alpha = F.softmax(e, dim=1)
         alpha = torch.transpose(alpha, dim0=2, dim1=1)
 
-        context = torch.squeeze(torch.matmul(alpha, tensor), dim=1)
-        context = torch.cat([context, char_ohe], dim=1)
+        context = torch.squeeze(torch.matmul(alpha, encoder_output), dim=1)
+        context = torch.cat([context, input], dim=1)
         context = torch.unsqueeze(context, dim=1)
 
-        output, hidden = self.rnn(context, hidden_0)
+        output, hidden = self.rnn(context, hidden)
         return output, hidden, alpha
